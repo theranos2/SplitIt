583ae13cd25fd0e8ec2326fc07a66c60b5e6e14b
Razin Idzuddin <razin99@gmail.com>
2022-06-27 13:14:04 +1000

    remove script, in favor of `dotnet tool restore` command

--- src/orm_setup.sh
+++
@@ -1,8 +0,0 @@
-dotnet new tool-manifest
-dotnet tool install dotnet-ef --version 5.0
-dotnet add package Microsoft.EntityFrameworkCore.Sqlite --vserion 5.0 
-dotnet add package Microsoft.EntityFrameworkCore.Design --version 5.0
-
-
-#dotnet ef migrations add "commit name"
-#dotnet ef database update  # used for creating database
