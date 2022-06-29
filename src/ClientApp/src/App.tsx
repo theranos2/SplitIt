51bbdf44b8069e489fae7f86c6536c32e9203a80
Lachlan <lachlanwaugh8@gmail.com>
2022-06-29 23:17:51 +1000

    Added a basic page not found page.

--- src/ClientApp/src/App.tsx
+++ src/ClientApp/src/App.tsx
@@ -7,14 +7,18 @@ import Register from './components/AccountManagement/Register';
 import BillAdvanced from './components/BillCreation/BillAdvanced';
 import BillSimple from './components/BillCreation/BillSimple';
 
+import NotFound from './components/NotFound';
+
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
+        {/* <Route exact path="/" element={<HomePage/>}/> */}
         <Route path="/login" element={<Login/>}/>
         <Route path="/register" element={<Register/>}/>
         <Route path="/bill/advanced" element={<BillAdvanced/>}/>
         <Route path="/bill/create" element={<BillSimple/>}/>
+        <Route path="*" element={<NotFound/>}/>
       </Routes>
     </BrowserRouter>
   );
---
+++ src/ClientApp/src/components/NotFound.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NotFound = () => {
+    return (
+        <div>
+            {/* <img src={PageNotFound}  /> */}
+            <p style={{textAlign:"center"}}>
+                <Link to="/">Go to Home </Link>
+            </p>
+        </div>
+    );
+};
+
+export default NotFound;
