1f0b10a6f1a25cde9bc44eb85b40b99212d2a3ea
Xibo Wu <xibo.wu@student.unsw.edu.au>
2022-07-17 20:48:04 +1000

    styled group view page

--- src/ClientApp/src/components/Groups/GroupsView/GroupsView.tsx
+++ src/ClientApp/src/components/Groups/GroupsView/GroupsView.tsx
@@ -1,17 +1,17 @@
 import * as React from 'react';
-import Link from '@mui/material/Link';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Title from './Title';
+import { Grid, Link, Paper } from '@mui/material';
 
-// Generate Order Data
 function createData(id: number, name: string, owner: string, members: string[]) {
   return { id, name, owner, members };
 }
 
+// TODO: Get groups from backend
 const rows = [
   createData(0, 'Minions fan club', 'Xibo', ['Adam']),
   createData(1, 'Lorem ipsum', 'Razin', ['Jingcheng', 'Ken']),
@@ -19,32 +19,35 @@ const rows = [
   createData(3, 'Theranos2', 'Jingcheng', ['Lachlan', 'Razin', 'Ken', 'Xibo', 'Adam'])
 ];
 
-function preventDefault(event: React.MouseEvent) {
-  event.preventDefault();
-}
-
 export default function Orders() {
   return (
-    <React.Fragment>
-      <Title>Your groups</Title>
-      <Table size="small">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell>Owner</TableCell>
-            <TableCell>Members</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow key={row.id}>
-              <TableCell>{row.name}</TableCell>
-              <TableCell>{row.owner}</TableCell>
-              <TableCell>{row.members.join(', ')}</TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </React.Fragment>
+    <Grid item xs={12}>
+      <Paper sx={{ p: 2, display: 'flex', flexDirection: 'column' }}>
+        <React.Fragment>
+          <Title>Your groups</Title>
+          <Table size="small">
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Owner</TableCell>
+                <TableCell>Members</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {rows.map((row) => (
+                <TableRow key={row.id}>
+                  <TableCell>{row.name}</TableCell>
+                  <TableCell>{row.owner}</TableCell>
+                  <TableCell>{row.members.join(', ')}</TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </React.Fragment>
+        <Link color="primary" href="/groups" sx={{ mt: 3 }}>
+          Go back
+        </Link>
+      </Paper>
+    </Grid>
   );
 }
