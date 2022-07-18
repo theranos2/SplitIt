0c7dddb1b0c6c198e30d7e664ae6340ef078c6b2
Razin Idzuddin <razin99@gmail.com>
2022-07-19 08:57:30 +1000

    implement seeder module

---
+++ seeder/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.IO;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
+using split_it;
+
+namespace seeder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dbFileName = "database.db";
+            Console.WriteLine("Creating context");
+            var path = Path.GetFullPath($"../src/{dbFileName}");
+            Console.WriteLine($"path={path}");
+            using var conn = new SqliteConnection($"Data Source={path.ToString()}");
+            conn.Open();
+            var ctxOpts = new DbContextOptionsBuilder<DatabaseContext>()
+                .UseSqlite(conn)
+                .Options;
+            using var ctx = new DatabaseContext(ctxOpts);
+            ctx.Database.EnsureCreated();
+            UserSeeder.Seed(ctx);
+            ctx.SaveChanges();
+        }
+    }
+}
