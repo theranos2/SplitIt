816fd032955f091b413093b03d8ccb1f8da1da1b
Lachlan <lachlanwaugh8@gmail.com>
2022-07-20 00:47:15 +1000

    cleaned up the item added page.

--- src/ClientApp/src/components/InputForm/ItemSelector/index.tsx
+++ src/ClientApp/src/components/InputForm/ItemSelector/index.tsx
@@ -8,6 +8,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import Button from '@mui/material/Button';
 import Alert from '@mui/material/Alert';
+import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 
 import ItemDisplay from 'components/Users/ItemDisplay';
@@ -50,26 +51,31 @@ export const ItemSelector = (props: ItemSelectorProps) => {
   // }, [currentItem]);
 
   return (
-    <div>
+    <>
       <Box component="form" sx={{ display: 'flex', flexWrap: 'wrap' }}>
         <FormControl sx={{ m: 1, minWidth: 120 }}>
-          <TextField
-            id="outlined-number"
-            label="Name"
-            type="text"
-            value={currentItem.name}
-            onChange={setItem('name')}
-            InputLabelProps={{ shrink: true }}
-          />
-          <TextField
-            id="outlined-number"
-            label="Price"
-            value={currentItem.price}
-            onChange={setItem('price')}
-            type="number"
-            InputLabelProps={{ shrink: true }}
-          />
-          {/* <InputLabel id={`select-${name}-inputlabel`}>Add {label}</InputLabel> */}
+          <Grid container spacing={1}>
+            <Grid item xs>
+              <TextField
+                id="outlined-number"
+                label="Name"
+                type="text"
+                value={currentItem.name}
+                onChange={setItem('name')}
+                InputLabelProps={{ shrink: true }}
+              />
+            </Grid>
+            <Grid item xs={6} sm={3} md>
+              <TextField
+                id="outlined-number"
+                label="Price"
+                value={currentItem.price}
+                onChange={setItem('price')}
+                type="number"
+                InputLabelProps={{ shrink: true }}
+              />
+            </Grid>
+          </Grid>
           <Select
             labelId={`select-${name}-label`}
             id={`select-${name}-id`}
@@ -84,12 +90,13 @@ export const ItemSelector = (props: ItemSelectorProps) => {
               </MenuItem>
             ))}
           </Select>
+          {/* <InputLabel id={`select-${name}-inputlabel`}>Add {label}</InputLabel> */}
         </FormControl>
       </Box>
-      <Button onClick={addItem}>Confirm</Button>
+      <Button onClick={addItem}>Add item</Button>
       {err.cond ? <Alert severity="warning">{err.msg}</Alert> : <></>}
       <ItemDisplay items={items} removeItem={removeItem} />
-    </div>
+    </>
   );
 };
 
