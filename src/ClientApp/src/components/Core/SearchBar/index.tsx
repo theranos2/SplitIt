a38d3f996f5ec01ffe00f527796a0b06bf817fb0
Lachlan <lachlanwaugh8@gmail.com>
2022-08-03 03:24:25 +1000

    updated the search bar to correctly filter the items.

--- src/ClientApp/src/components/Core/SearchBar/index.tsx
+++ src/ClientApp/src/components/Core/SearchBar/index.tsx
@@ -1,21 +1,47 @@
 import { useState } from 'react';
 import TextField from '@mui/material/TextField';
 import SearchIcon from '@mui/icons-material/Search';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
-import SearchBarProps, { FilterProps, OrderProps } from './props';
+import SearchBarProps, { FilterProps } from './props';
 
 const SearchBar = (props: SearchBarProps) => {
-  const { items, currentItems, setCurrentItems, filters } = props;
+  const { items, setCurrentItems, filters } = props;
 
   const [filter, setFilter] = useState<FilterProps>({ name: '', value: undefined });
-  const [order, setOrder] = useState<OrderProps>({ name: '' });
 
-  const checkFilter = (filter: FilterProps) => filters[filter.name];
+  const filterUsers = () => setCurrentItems(filters[filter.name](items, filter.value));
 
   return (
     <>
-      <TextField id="standard-bare" defaultValue="Bare" margin="normal" />
+      <TextField
+        id="standard-bare"
+        value={filter.value}
+        onChange={(e: any) => {
+          setFilter((old: FilterProps) => {
+            return { name: old.name, value: e.target.value };
+          });
+          filterUsers();
+        }}
+        margin="normal"
+      />
       <SearchIcon />
+      <InputLabel id="demo-simple-select-label">Filter type</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={filter.name}
+        label="Select filter"
+        onChange={(e: any) => setFilter({ name: e.target.value, value: '' })}
+      >
+        {Object.keys(filters).map((filter: string, idx: number) => (
+          <MenuItem key={`filter-${idx}`} value={filter}>
+            {filter}
+          </MenuItem>
+        ))}
+      </Select>
     </>
   );
 };
--- src/ClientApp/src/components/Core/SearchBar/props.tsx
+++ src/ClientApp/src/components/Core/SearchBar/props.tsx
@@ -2,7 +2,6 @@ import { BillDto } from 'api';
 
 export default interface SearchBarProps {
   items: BillDto[];
-  currentItems: BillDto[];
   setCurrentItems: (bill: BillDto[]) => void;
   filters: Record<string, <T>(bills: T[], value: any) => T[]>;
 }
@@ -10,8 +9,4 @@ export default interface SearchBarProps {
 export interface FilterProps {
   name: string;
   value: any;
-}
-
-export interface OrderProps {
-  name: string;
 }
