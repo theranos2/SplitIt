5e271e951ce0548f503f345b97ca2facb0959d70
Xibo Wu <xibo.wu@student.unsw.edu.au>
2022-08-02 22:41:47 +1000

    single group view

--- src/ClientApp/src/components/Groups/GroupView/GroupView.tsx
+++ src/ClientApp/src/components/Groups/GroupView/GroupView.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
-import { Container, createTheme, Link, ThemeProvider } from '@mui/material';
-import { Title } from '@mui/icons-material';
+import { Box, Container, createTheme, Link, ThemeProvider, Typography } from '@mui/material';
+import { Groups, Title } from '@mui/icons-material';
 import { DetailedGroupDto, GroupApi } from 'api';
 import { useParams } from 'react-router-dom';
 import { token } from 'utility/config';
@@ -48,15 +48,38 @@ export default function GroupView() {
     );
   }
 
+  // Initialise and populate group members array
+  var members: string = '';
+  if (group.members != undefined) {
+    members = group.members.map(m => m.firstName + ' ' + m.lastName).join(', ');
+  }
+
   return (
     <ThemeProvider theme={theme}>
-      <Container component="main" maxWidth="xs">
-        <Title>Viewing group {group.name}</Title>
-        <Link color="primary" href="/groups/view" sx={{ mt: 3 }}>
-            Go back
-        </Link>
-      </Container>
-    </ThemeProvider>
-  );
+      {/* Hero unit */}
+      <Box
+        sx={{
+          bgcolor: 'background.paper',
+          pt: 8,
+          pb: 6,
+        }}
+      >
+        <Container maxWidth="sm">
+          <Typography
+            component="h1"
+            variant="h2"
+            align="center"
+            color="text.primary"
+            gutterBottom
+          >
+            {group.name}
+          </Typography>
+          <Typography variant="h5" align="center" color="text.secondary" paragraph>
+            Owner: {group.owner?.firstName + ' ' + group.owner?.lastName}
+            <br></br>
+            Members: {members}
+          </Typography>
+        </Container>
+      </Box>
+    </ThemeProvider>  );
 }
-
